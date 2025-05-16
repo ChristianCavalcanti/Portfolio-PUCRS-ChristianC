32676abd2243248180189732be03d6ef92bb4185
ChristianCavalcanti <c.cavalcanti@live.com>
2025-05-16 20:20:41 -0300

    screen updates

--- ExpressServer/src/index.js
+++ ExpressServer/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const rotaUsuario = require('./rotas/usuario.rota');
 const rotaPost = require('./rotas/post.rota');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 app.use('/usuarios', rotaUsuario);
 app.use('/posts', rotaPost);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.json({ msg: "Hello from Express!" });

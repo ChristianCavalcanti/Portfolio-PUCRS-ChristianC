9a4eed533fce076e3b00c9ca995ac2f67f081654
ChristianCavalcanti <c.cavalcanti@live.com>
2025-05-16 20:41:35 -0300

    a

--- ReactNativeProject/DogeCat/App.js
+++ ReactNativeProject/DogeCat/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BlogPost from './pages/post';
 import Home from './pages/home';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Text style={styles.container}> Hello Testororororo</Text>
-        <Stack.Screen name="Home" component={Home} options={{headerShown: false}}></Stack.Screen>
-        <Stack.Screen name="BlogPost" component={BlogPost} options={{headerShown: false}}></Stack.Screen>
-    </Stack.Navigator>
-</NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={styles.container}>Hello Testororororo</Text>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="BlogPost" component={BlogPost} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
-  container:  {
-    backgroundColor: '0F0'
+  container: {
+    backgroundColor: '#0F0',
+    padding: 10
   }
-
-})
+});
